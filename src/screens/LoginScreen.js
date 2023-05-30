5df89287a04bf936bd7d4c7fe3d1033215b358b3
sashazakatov <58004989+sashazakatov@users.noreply.github.com>
2023-05-30 18:02:53 +0300

    add files with goit-react-native-hw-3

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,180 @@
+import { useEffect, useState, useReducer } from 'react';
+import { 
+    Pressable, 
+    Text, 
+    TextInput, 
+    View, 
+    StyleSheet, 
+    ImageBackground, 
+    Keyboard, 
+    TouchableWithoutFeedback,
+    Button,
+    TouchableOpacity,
+    Alert
+} from 'react-native';
+import InputPassword from '../companents/InputPassword';
+
+const initialState = {
+    email: '',
+    password: '',
+}
+
+function reducer(state, action) {
+    switch (action.type){
+        case 'registerInformation/email':
+            return { ...state, email: action.payload }
+        case 'registerInformation/password':
+            return { ...state, password: action.payload }
+        case 'registerInformation/reset':
+            return initialState;
+        default:
+            Alert.alert('Data entry error, invalid input field');
+            return state;
+    }
+}
+
+const LoginScreen = () => {
+    const [isKeyboardOpen, setIsKeyboardOpen] = useState(true);
+    const [registerInformation, setRegisterInformation] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+      const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', () => {
+        setIsKeyboardOpen(true);
+      });
+  
+      const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', () => {
+        setIsKeyboardOpen(false);
+      });
+  
+      return () => {
+        keyboardDidShowListener.remove();
+        keyboardDidHideListener.remove();
+      };
+    }, []);
+
+
+    const henadelSubmit = () => {
+        const { email, password } = registerInformation;
+
+        if( email === '' || password === ''){
+            return Alert.alert('All fields must be filled');
+        }
+
+        console.log(`email: ${email}, password: ${password}`);
+
+        setRegisterInformation({ type: 'registerInformation/reset' });
+    }
+
+    return(
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={style.form}>
+                <View style={style.photo}>
+                    {/* <Button title='add'/> */}
+                </View>
+            <Text style={style.title}>Реєстрація</Text>
+            <TextInput
+                style={style.input}
+                placeholder='Адреса електронної пошти'
+                keyboardType='email-address'
+                value={registerInformation.email}
+                onChangeText={(payload) => 
+                    setRegisterInformation({type: 'registerInformation/email', payload})
+            }
+            />
+            <InputPassword 
+                password={registerInformation.password} 
+                dispatch={setRegisterInformation}
+            />
+            { !isKeyboardOpen && (
+                <>
+                    <Pressable style={style.button} onPress={henadelSubmit}>
+                        <Text style={{color: '#FFFFFF', fontSize: 16, }}>Нажми меня</Text>
+                    </Pressable>
+                    <Text style={style.text}>Немає акаунту? Зареєструватися</Text>
+                </>
+            )}
+        </View>
+    </TouchableWithoutFeedback>
+    );
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        height: '100%',
+        width: '100%',
+    },
+    form: {
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+
+        alignItems: 'center',
+
+        paddingTop: 92,
+        paddingLeft: 16,
+        paddingRight: 16,
+    },
+    photo: {
+        position: 'absolute',
+        backgroundColor: '#F6F6F6',
+        width: 120,
+        height: 120,
+        borderRadius: 16,
+        marginBottom:32,
+        transform: [{ translateY: -50 }],
+    },
+    title: {
+        textAlign: 'center',
+        fontFamily: 'Roboto',
+        marginBottom: 33,
+        fontSize: 30,
+    },
+    input: {
+        height: 40,
+        width: '100%',
+        backgroundColor: 'whitesmoke',
+        borderWidth: 1,
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 16,
+    },
+    button: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 40,
+        width: '100%',
+        fontFamily: 'Roboto',
+        color: '#FFFFFF',
+        backgroundColor: "#FF6C00",
+        borderRadius: 100,
+        marginBottom: 16,
+    },
+    text:{
+        fontFamily: 'Roboto',
+        fontWeight: 400,
+        fontSize: 16,
+        color: "#1B4371",
+        marginBottom: 40,
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        position: 'relative',
+    },
+    inputButton: {
+        position: 'absolute',
+        top: 10,
+        right: 16,
+    },
+    inputPassword:{
+        flex: 1,
+        height: 40,
+        paddingHorizontal: 10,
+        height: '100%',
+    },
+});
+export default LoginScreen;
